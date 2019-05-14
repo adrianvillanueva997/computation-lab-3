fa03f6b60b54fa5257f658c6468412c6ec92c7df
Miguel Chacón <michaca94@gmail.com>
2019-05-14 14:08:37 +0200

    Tabla propuestas de usuario

--- resources/views/user/user_panel/verpropuesta.blade.php
+++ resources/views/user/user_panel/verpropuesta.blade.php
@@ -3,8 +3,25 @@
     <div class="row">
         <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
         <div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
-            <h2>Aquí estan tus propuestas:</h2>
-            <p>Aquí se mostraran las propuestas</p>
+                <h2>Aquí estan tus propuestas:</h2>
+                <table class="table">
+                    <thead>
+                        <tr>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Descripción</th>
+                        <th scope="col">Tags</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($propuestas as $propuesta)
+                            <tr>
+                                <td>{!! $propuesta->name !!}</td>
+                                <td>{!! $propuesta->description !!}</td>
+                                <td>{!! $propuesta->tags !!}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
         </div>
         <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
     </div>
