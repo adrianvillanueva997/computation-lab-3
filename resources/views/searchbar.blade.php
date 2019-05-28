3d753807dd8f9663ee9f4ec23a3e3f2f753bcac1
ad2946 <adrigarort@gmail.com>
2019-05-28 13:13:02 +0200

    Cambios searchbar

--- resources/views/searchbar.blade.php
+++ resources/views/searchbar.blade.php
@@ -4,7 +4,7 @@
           <form class="form-inline center-block" action="/buscar_propuestas" method="POST" role="search">
           {{ csrf_field() }}
             <div class="input-group">
-              <input type="search" class="form-control" id="search" name="searchtext" size="50" placeholder="Search..." required>
+              <input type="search" class="form-control" id="search" name="searchtext" size="50" placeholder="¿Qué necesitas?" required>
               <div class="input-group-btn">
                 <button type="submit" class="btn btn-danger">
                     <i class="fa fa-search" style='font-size:20px'></i>
