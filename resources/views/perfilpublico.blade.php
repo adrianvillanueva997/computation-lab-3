7994fa787d4ef2b8ca015217bd2d69a3dff894e1
ad2946 <adrigarort@gmail.com>
2019-06-04 12:19:28 +0200

    Perfil de usuario

--- resources/views/perfilpublico.blade.php
+++ resources/views/perfilpublico.blade.php
@@ -26,7 +26,7 @@
     <hr>
 <div class="container bootstrap snippet">
     <div class="row">
-  		<div class="col-sm-10"><h1>Ususuario</h1></div>
+  		<div class="col-sm-10"><h1>{{$user->name}}</h1></div>
 <!--
     	<div class="col-sm-2"><a href="/users" class="pull-right">
     	<img title="profile image" class="img-circle img-responsive" src="http://www.gravatar.com/avatar/28fd20ccec6865e2d5f0e1f4446eb7bf?s=100"></a></div>
@@ -86,6 +86,82 @@
                       <div class="form-group">
                           
                           <div class="col-xs-6">
+                              <label for="first_name"><h4>Nombre</h4></label><br>
+                              <label for="nombre"><h4>{{$user->name}}</label>                          
+                          </div>
+                      </div>
+                      <div class="form-group">
+                          
+                          <div class="col-xs-6">
+                              <label for="first_name"><h4>Email</h4></label><br>
+                              <label for="nombre"><h4>{{$user->email}}</label>                          
+                          </div>
+                      </div>
+                      <div class="form-group">
+                          
+                          <div class="col-xs-6">
+                              <label for="first_name"><h4>Edad</h4></label><br>
+                              <label for="nombre"><h4>{{$user->age}}</label>                          
+                          </div>
+                      </div>
+                      <div class="form-group">
+                          
+                          <div class="col-xs-6">
+                              <label for="first_name"><h4>Descripción</h4></label><br>
+                              <label for="nombre"><h4>{{$user->description}}</label>                          
+                          </div>
+                      </div>
+                      <div class="form-group">
+                          
+                          <div class="col-xs-6">
+                              <label for="first_name"><h4>Ocupación</h4></label><br>
+                              <label for="nombre"><h4>{{$user->ocupation}}</label>                          
+                          </div>
+                      </div>
+                      <div class="form-group">
+                          
+                          <div class="col-xs-6">
+                              <label for="first_name"><h4>Habilidades</h4></label><br>
+                              <label for="nombre"><h4>{{$user->habilities}}</label>                          
+                          </div>
+                      </div>
+                      
+              	</form>
+              
+              <hr>
+              
+             </div><!--/tab-pane-->
+             <div class="tab-pane" id="Propuestas">
+               
+               <h2></h2>
+               <hr>
+               @foreach($propuestas as $propuesta)
+                <div class="col-lg-4 col-sm-6 mb-4">
+                    <div class="card h-100">
+                      <a href="/propuesta/{{$propuesta->id}}"><img class="card-img-top" src="{{asset("images/$propuesta->image_path")}}" alt=""></a>
+                        <div class="card-body">
+                                <h4 class="card-title">
+                                <a href="/propuesta/{{$propuesta->id}}">{{$propuesta->name}}</a>
+                                </h4>
+                                <h6 class="card-text font-weight-bold">Descripcion</h6>
+                                <p class="card-text">{{$propuesta->description}}</p>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
+                <div class="row">
+                {{$propuestas->links()}}
+                </div>
+               
+             </div><!--/tab-pane-->
+             <div class="tab-pane" id="settings">
+            		
+               	
+                  <hr>
+                  <form class="form" action="##" method="post" id="registrationForm">
+                      <div class="form-group">
+                          
+                          <div class="col-xs-6">
                               <label for="first_name"><h4>First name</h4></label>
                               <input type="text" class="form-control" name="first_name" id="first_name" placeholder="first name" title="enter your first name if any.">
                           </div>
@@ -116,48 +192,41 @@
                           
                           <div class="col-xs-6">
                               <label for="email"><h4>Email</h4></label>
-                              <label for="email"><h4>Email</h4></label>
+                              <input type="email" class="form-control" name="email" id="email" placeholder="you@email.com" title="enter your email.">
                           </div>
                       </div>
                       <div class="form-group">
                           
                           <div class="col-xs-6">
-                              <label for="email"><h4>Localización</h4></label>
-                              <label for="email"><h4>Localización</h4></label>
+                              <label for="email"><h4>Location</h4></label>
+                              <input type="email" class="form-control" id="location" placeholder="somewhere" title="enter a location">
                           </div>
                       </div>
-                     
-                     
-                   
+                      <div class="form-group">
+                          
+                          <div class="col-xs-6">
+                              <label for="password"><h4>Password</h4></label>
+                              <input type="password" class="form-control" name="password" id="password" placeholder="password" title="enter your password.">
+                          </div>
+                      </div>
+                      <div class="form-group">
+                          
+                          <div class="col-xs-6">
+                            <label for="password2"><h4>Verify</h4></label>
+                              <input type="password" class="form-control" name="password2" id="password2" placeholder="password2" title="enter your password2.">
+                          </div>
+                      </div>
+                      <div class="form-group">
+                           <div class="col-xs-12">
+                                <br>
+                              	<button class="btn btn-lg btn-success pull-right" type="submit"><i class="glyphicon glyphicon-ok-sign"></i> Save</button>
+                               	<!--<button class="btn btn-lg" type="reset"><i class="glyphicon glyphicon-repeat"></i> Reset</button>-->
+                            </div>
+                      </div>
               	</form>
-              
-              <hr>
-              
-             </div><!--/tab-pane-->
-             <div class="tab-pane" id="Propuestas">
+              </div>
                
-               <h2></h2>
-               <hr>
-               @foreach($propuestas as $propuesta)
-                <div class="col-lg-4 col-sm-6 mb-4">
-                    <div class="card h-100">
-                      <a href="/propuesta/{{$propuesta->id}}"><img class="card-img-top" src="{{asset("images/$propuesta->image_path")}}" alt=""></a>
-                        <div class="card-body">
-                                <h4 class="card-title">
-                                <a href="/propuesta/{{$propuesta->id}}">{{$propuesta->name}}</a>
-                                </h4>
-                                <h6 class="card-text font-weight-bold">Descripcion</h6>
-                                <p class="card-text">{{$propuesta->description}}</p>
-                        </div>
-                    </div>
-                </div>
-                @endforeach
-                <div class="row">
-                {{$propuestas->links()}}
-                </div>
-               
-             </div><!--/tab-pane-->
-            
+              </div><!--/tab-pane-->
           </div><!--/tab-content-->
 
         </div><!--/col-9-->
